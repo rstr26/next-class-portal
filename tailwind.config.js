1ceec72b21f3a259f9ad8171bb49fd640ebdfe86
rstr26 <tabernillad3rd@gmail.com>
2024-03-12 22:32:12 +0800

    completed setting up tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.js', 
+    './src/modules/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
