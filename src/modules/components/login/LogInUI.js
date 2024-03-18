9a1f17faee01703fd652739242ed8fbb18f01c2f
rstr26 <tabernillad3rd@gmail.com>
2024-03-18 20:59:35 +0800

    log in desktop ui

--- src/modules/components/login/LogInUI.js
+++ src/modules/components/login/LogInUI.js
@@ -1,14 +1,34 @@
+import Button from '@/modules/shared/tailwind-components/Button'
+import Input from '@/modules/shared/tailwind-components/Input'
+import { ArrowLeftStartOnRectangleIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import React from 'react'
 
 const LogInUI = () => {
     return (
         <div className='flex'>
-            <div className='flex-1 bg-primary h-screen'>
-                <h1>Log In</h1>
+            <div className='w-2/5 bg-primary h-screen flex justify-center items-center'>
+                <form>
+                    <h1 className='text-white font-bold text-4xl mb-2'>Login</h1>
+                    <p className='text-xs text-white mb-6'>Enter your credentials below.</p>
+
+                    <p className='text-white'>Username</p>
+                    <input className='outline-none bg-transparent border-b-2 text-white text-xl' />
+                    <p className='text-white mt-5'>Password</p>
+                    <input className='outline-none bg-transparent border-b-2 text-white text-xl' type='password' />
+
+                    <Button 
+                        text='Login' 
+                        type='secondary' 
+                        size='md' fluid 
+                        style={{ marginTop: '20px' }}
+                        icon={<ArrowLeftStartOnRectangleIcon />}
+                    />
+                    <a className='text-white text-xs font-body underline' href=''>Sign up for an account.</a>
+                </form>
             </div>
 
-            <div className='flex-1 relative flex justify-center'>
+            <div className='w-3/5 relative flex justify-center'>
                 <div className='absolute top-28'>
                     <h1 
                         className='text-3xl font-bold font-body'
@@ -23,6 +43,8 @@ const LogInUI = () => {
                     height={500}
                     title='Image by pch.vector on Freepik'
                     className='absolute top-44'
+                    alt='Students vector'
+                    priority
                 />
             </div>
         </div>
--- src/modules/shared/tailwind-components/Button.js
+++ src/modules/shared/tailwind-components/Button.js
@@ -3,12 +3,12 @@ import React, { useEffect } from 'react'
 import { TailwindUtils } from '../sharedFunctions'
 import PropTypes from 'prop-types'
 
-const Button = ({ text, size, inverted, icon, type, onClick }) => {
+const Button = ({ text, size, inverted, icon, type, onClick, fluid, style }) => {
     const combinedClasses = clsx(
         {
-            'flex items-center rounded-md my-2 py-1 px-3 font-sm transition duration-300 text-white':
+            'flex justify-center items-center rounded-md my-2 py-1 px-3 font-sm transition duration-300 text-white':
             !inverted,
-            'flex items-center border-2 rounded-md my-2 py-1 px-3 font-sm transition duration-300': 
+            'flex justify-center items-center border-2 rounded-md my-2 py-1 px-3 font-sm transition duration-300': 
             inverted
         }, 
         {
@@ -16,7 +16,14 @@ const Button = ({ text, size, inverted, icon, type, onClick }) => {
             [TailwindUtils('inverted btn border', { inverted: inverted, size: size })]: true,
             [TailwindUtils('bg', { bg: type })]: !inverted && true,
             [TailwindUtils('hover bg', { bg: type })]: true,
+            'w-full': fluid,
             'hover:text-white': inverted
+        }
+    )
+    const iconClasses = clsx(
+        'w-5 h-5 mr-1',
+        {
+            'hidden': !icon
         }
     )
 
@@ -25,8 +32,9 @@ const Button = ({ text, size, inverted, icon, type, onClick }) => {
         <button 
             className={combinedClasses}
             onClick={onClick}
+            style={style}
         >
-            <div className='w-5 h-5 mr-1'>
+            <div className={iconClasses}>
                 {icon && icon}
             </div>
             {text}
@@ -40,7 +48,9 @@ Button.propTypes = {
     inverted: PropTypes.bool,
     icon: PropTypes.element,
     type: PropTypes.oneOf(['primary', 'secondary', 'positive', 'negative', 'neutral']),
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    fluid: PropTypes.bool,
+    style: PropTypes.object
 }
 
 export default Button
