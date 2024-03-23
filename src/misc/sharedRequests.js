f73acd57c91792d7ffdde2cee26b9aa74d48a4e9
rstr26 <tabernillad3rd@gmail.com>
2024-03-23 22:58:56 +0800

    login backend/jwt wip, added axios

---
+++ src/misc/sharedRequests.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+import { ApiURL, HeaderConfig } from "./apiconfig"
+
+const key = process.env.NEXT_PUBLIC_API_KEY
+let body = {}
+
+export async function Login(credentials){
+    body = {
+        key: key,
+        ...credentials
+    }
+
+    const res = await axios.post(`${ApiURL()}/login`, body, HeaderConfig())
+    .then((res) => res)
+    .catch((err) => { return { data: err } })
+
+    return res
+}
