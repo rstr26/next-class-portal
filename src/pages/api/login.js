a91bbabe825d8edf2f2444482364b6cdad1ad37c
rstr26 <tabernillad3rd@gmail.com>
2024-03-24 22:46:34 +0800

    fixed api to db connection

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,13 +1,47 @@
+import { config } from '@/misc/apiconfig'
+const sql = require('mssql')
 const jwt = require('jsonwebtoken')
 const apikey = process.env.NEXT_PUBLIC_API_KEY
 const accesskey = process.env.NEXT_PUBLIC_ACCESS_TOKEN_KEY
+
+
+export async function ExecuteRecordsetQry(qry){
+    return new Promise((resolve, reject) => {
+        let response = (e, m) => { return { error: e, message: m } }
+
+        sql.connect(config, (err) => {
+            if(err) reject(response(true, err))
+
+            const req = new sql.Request()
+
+            req.query(qry, (err, recset) => {
+                if(err) reject(response(true, err))
+
+                resolve(response(false, recset))
+            })
+        })
+    })
+}
 
 export default function handler(req, res){
     const { key, user, pw } = req.body
 
     if(key === apikey){
+        const qry =
+        `
+        SELECT * FROM user_accounts
+        `
+
+        ExecuteRecordsetQry(qry)
+        .then(({ message, error }) => {
+            res.json({ error: error, message: message.recordset })
+        })
+        .catch((err) => {
+            console.log(err);
+            res.json({ error: true })
+        })
         const accessToken = jwt.sign({ user: user }, accesskey)
-        res.json({ error: false, access: accessToken })
+        // res.json({ error: false, access: accessToken })
     }
     else res.json({ error: true })
 }
